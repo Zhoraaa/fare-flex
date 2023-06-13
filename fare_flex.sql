5879ede2f4a9fb36b02bae56ef84ca3274ce94bf
zh0raaa <102014678+Zhoraaa@users.noreply.github.com>
2023-06-13 20:44:48 +0500

    ver 0.1.1
    + new DB
    + taxist forms

---
+++ fare_flex.sql
@@ -0,0 +1,344 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 13 2023 г., 18:44
+-- Версия сервера: 8.0.30
+-- Версия PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `fare_flex`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `cars`
+--
+
+CREATE TABLE `cars` (
+  `id` int NOT NULL,
+  `model` varchar(128) NOT NULL,
+  `number` text NOT NULL,
+  `color` varchar(128) NOT NULL,
+  `type` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `cars`
+--
+
+INSERT INTO `cars` (`id`, `model`, `number`, `color`, `type`) VALUES
+(1, 'Требуется авто', 'Требуется авто', 'Требуется авто', 1),
+(2, 'ВАЗ 2501', 'п219ок', 'Жёлтый', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `car_types`
+--
+
+CREATE TABLE `car_types` (
+  `id` int NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `car_types`
+--
+
+INSERT INTO `car_types` (`id`, `name`) VALUES
+(1, 'Имущество таксопарка'),
+(2, 'Личное авто');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int NOT NULL,
+  `taxist` int NOT NULL,
+  `client` int NOT NULL,
+  `from` int NOT NULL,
+  `to` int NOT NULL,
+  `grade_taxist` int NOT NULL,
+  `grade_client` int NOT NULL,
+  `status` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `orders`
+--
+
+INSERT INTO `orders` (`id`, `taxist`, `client`, `from`, `to`, `grade_taxist`, `grade_client`, `status`) VALUES
+(1, 1, 5, 1231, 123123, 5, 5, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `roles`
+--
+
+INSERT INTO `roles` (`id`, `name`) VALUES
+(1, 'Админ'),
+(2, 'Таксист'),
+(3, 'Клиент');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `status`
+--
+
+CREATE TABLE `status` (
+  `id` int NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `status`
+--
+
+INSERT INTO `status` (`id`, `name`) VALUES
+(1, 'Новый'),
+(2, 'В исполнении'),
+(3, 'Завершён'),
+(4, 'Отменён');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `taxists`
+--
+
+CREATE TABLE `taxists` (
+  `id` int NOT NULL,
+  `photo` varchar(128) NOT NULL,
+  `car` int NOT NULL,
+  `user` int NOT NULL,
+  `passport` bigint NOT NULL,
+  `driver_license` bigint NOT NULL,
+  `status` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `taxists`
+--
+
+INSERT INTO `taxists` (`id`, `photo`, `car`, `user`, `passport`, `driver_license`, `status`) VALUES
+(1, 'default.jpg', 2, 6, 8018790994, 8018790994, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `taxist_status`
+--
+
+CREATE TABLE `taxist_status` (
+  `id` int NOT NULL,
+  `name` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `taxist_status`
+--
+
+INSERT INTO `taxist_status` (`id`, `name`) VALUES
+(1, 'Работает'),
+(2, 'Ожидает ответа'),
+(3, 'В чёрном списке');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `login` varchar(128) NOT NULL,
+  `email` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `role` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `login`, `email`, `password`, `role`) VALUES
+(4, 'Админ', 'admin', 'admin@admin', 'e020590f0e18cd6053d7ae0e0a507609', 1),
+(5, 'Максим', 'maxim', 'max@max', '0aa77bd190a1be0b87e7591b6bd72d88', 2),
+(6, 'Учбучбек', 'oochBCHbeck', 'oochBCH@beck', 'efe6398127928f1b2e9ef3207fb82663', 3);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `type` (`type`);
+
+--
+-- Индексы таблицы `car_types`
+--
+ALTER TABLE `car_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `status` (`status`),
+  ADD KEY `taxist` (`taxist`,`client`,`from`,`to`,`status`),
+  ADD KEY `client` (`client`);
+
+--
+-- Индексы таблицы `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `taxists`
+--
+ALTER TABLE `taxists`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`),
+  ADD KEY `car` (`car`),
+  ADD KEY `status` (`status`);
+
+--
+-- Индексы таблицы `taxist_status`
+--
+ALTER TABLE `taxist_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `role` (`role`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT для таблицы `car_types`
+--
+ALTER TABLE `car_types`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `status`
+--
+ALTER TABLE `status`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `taxists`
+--
+ALTER TABLE `taxists`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `taxist_status`
+--
+ALTER TABLE `taxist_status`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `cars`
+--
+ALTER TABLE `cars`
+  ADD CONSTRAINT `cars_ibfk_1` FOREIGN KEY (`type`) REFERENCES `car_types` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`taxist`) REFERENCES `taxists` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`status`) REFERENCES `status` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`client`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `taxists`
+--
+ALTER TABLE `taxists`
+  ADD CONSTRAINT `taxists_ibfk_1` FOREIGN KEY (`user`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `taxists_ibfk_2` FOREIGN KEY (`car`) REFERENCES `cars` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `taxists_ibfk_3` FOREIGN KEY (`status`) REFERENCES `taxist_status` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Ограничения внешнего ключа таблицы `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role`) REFERENCES `roles` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- formNewTaxist.php
+++ formNewTaxist.php
@@ -4,22 +4,28 @@ require "./pageBase.php";
 <main>
   <form action="" method="post" enctype="multipart/form-data">
     <h2>Заявка на работу таксистом</h2>
-    <div><input type="file" name="photo" placeholder=""></div>
+    <h3>Ваше фото. (Оображается при отклике)</h3>
+    <div><input type="file" name="photo"></div>
     <h3>Паспорт</h3>
-    <div><input type="text" name="passport" placeholder="XX XX XXXXXX"></div>
+    <div><input type="file" name="passport"></div>
     <h3>Водительское удостоверение</h3>
-    <div><input type="text" name="driver" placeholder="XX XX XXXXXX"></div>
-    <h3>ПТС</h3>
-    <div><input type="text" name="transport" placeholder="XX XX XXXXXX"></div>
+    <div><input type="file" name="driver"></div>
+    <div id="infoCar">
+      <?php include "taxist/form-car.html" ?>
+    </div>
     <div>
       <label for="haveCar">
         <input type="checkbox" name="haveCar" id="haveCar">
-        <span>Есть личное авто</span>
+        <span>Нет личного авто</span>
       </label>
     </div>
-    <div id="infoCar">
-
+    <div>
+      <label for="perData">
+        <input type="checkbox" name="perData" id="perData">
+        <span>Даю своё согласие на обработку персональных данных.</span>
+      </label>
     </div>
+    <div><button class="pad15 brad15 box-shadow">Подать заявку</button></div>
   </form>
 </main>
 
@@ -28,9 +34,9 @@ require "./pageBase.php";
   var haveCar = document.querySelector('#haveCar');
   haveCar.addEventListener('change', () => {
     if (haveCar.checked) {
-      asyncLoad("taxist/form-car.html", "infoCar");
+      asyncClear("infoCar");
     } else {
-      asyncClear("test");
+      asyncLoad("taxist/form-car.html", "infoCar");
     }
   })
 </script>
